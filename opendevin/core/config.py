fcbaeb4d16dab9747f7cf0207766a2f633382074
Dane Sherburn <danesherbs@gmail.com>
2024-06-25 14:57:59 +0100

    Update config.py

--- opendevin/core/config.py
+++ opendevin/core/config.py
@@ -109,7 +109,7 @@ class AgentConfig(metaclass=Singleton):
         memory_max_threads: The maximum number of threads indexing at the same time for embeddings.
     """
 
-    name: str = 'PlannerAgent'
+    name: str = 'CodeActAgent'
     memory_enabled: bool = False
     memory_max_threads: int = 2
 
